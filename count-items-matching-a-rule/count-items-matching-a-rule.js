80ecb726572765a503820228ba6a472869643552
Kaan Sinar <56046728+ksinar@users.noreply.github.com>
2021-08-25 02:28:17 -0400

    Time: 88 ms (69.11%), Space: 42.7 MB (42.20%) - LeetHub

---
+++ count-items-matching-a-rule/count-items-matching-a-rule.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let count = 0;
+    for(let i = 0; i < items.length; i++){
+        if(ruleKey == "type" && items[i][0] == ruleValue){
+            count++;
+        }else if(ruleKey == "color" && items[i][1] == ruleValue){
+            count++;
+        }else if(ruleKey == "name" && items[i][2] == ruleValue){
+            count++;
+        }
+    }
+    return count;
+};
